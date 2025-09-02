cc2b3b9323e7f365c8bcb11f055b7a4b4222afb5
AndyCort <1771858268@qq.com>
2025-09-03 03:15:40 +0800

    update

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, veiwport-fit=cover">
     <meta name="description" content="<?php bloginfo('description'); ?>">
     <title><?php wp_title('|', true, 'right'); ?></title>
     <script>
