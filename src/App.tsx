0c2b66cdf29c5d0c60cb2b0e20aae1aae2574f36
AndyCort <1771858268@qq.com>
2026-09-02 19:13:11 +0800

    breaking update

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './styles/style.css';
 import { initTheme } from './stores/themeStore';
 
-import Home from './pages/Home';
-import About from './pages/About';
-import Articles from './pages/Articles';
-import Search from './pages/Search';
-import ArticleDetail from './pages/ArticleDetail';
-import Moments from './pages/Moments';
+const Home = lazy(() => import('./pages/Home'));
+const About = lazy(() => import('./pages/About'));
+const Articles = lazy(() => import('./pages/Articles'));
+const Search = lazy(() => import('./pages/Search'));
+const ArticleDetail = lazy(() => import('./pages/ArticleDetail'));
+const Moments = lazy(() => import('./pages/Moments'));
 
 initTheme();
 
@@ -26,14 +26,16 @@ class ErrorBoundary extends React.Component<{ children: React.ReactNode }, { has
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/search" element={<Search />} />
-      <Route path="/articles" element={<Articles />} />
-      <Route path="/articles/:slug" element={<ArticleDetail />} />
-      <Route path="/moment" element={<Moments />} />
-    </Routes>
+    <Suspense fallback={<div style={{ minHeight: '100vh' }} />}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/articles" element={<Articles />} />
+        <Route path="/articles/:slug" element={<ArticleDetail />} />
+        <Route path="/moment" element={<Moments />} />
+      </Routes>
+    </Suspense>
   );
 }
 
